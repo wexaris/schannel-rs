f32ea039fed47803602dbe73d2df98adca835e7b
Rainers Vorza <rainersvorza@gmail.com>
2023-03-06 21:29:42 +0200

    Workaround for ContextBuffer pointer being NULL

--- src/context_buffer.rs
+++ src/context_buffer.rs
@@ -17,6 +17,11 @@ impl Deref for ContextBuffer {
     type Target = [u8];
 
     fn deref(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts(self.0.pvBuffer as *const _, self.0.cbBuffer as usize) }
+        if !self.0.pvBuffer.is_null() {
+            unsafe { slice::from_raw_parts(self.0.pvBuffer as *const _, self.0.cbBuffer as usize) }
+        }
+        else {
+            unsafe { slice::from_raw_parts(std::ptr::NonNull::dangling().as_ptr(), self.0.cbBuffer as usize) }
+        }
     }
 }
